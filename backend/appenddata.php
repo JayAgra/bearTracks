a6f269d1112296bc3310c23ef4683a053dd44dda
Jayen Agrawal <69493224+JayAgra@users.noreply.github.com>
2022-04-04 12:14:07 -0700

    Update appenddata.php

--- backend/appenddata.php
+++ backend/appenddata.php
@@ -1,7 +1,19 @@
 <?php
 if (isset($_POST['entry_match'])) {
-  if ($_POST['entry_key'] === 'TEAMKEYHERE') {
+  if ($_POST['entry_key'] === 'ADMINKEY') {
+    $target = $_POST['entry_match'];
+    class formDB extends SQLite3
+     {
+        function __construct()
+           {
+             $this->open('data.db');
+           }
+     }
+    $db = new formDB();
+    //execute
     
+    //close DB
+    $db->close();
   }
 }
 ?>
