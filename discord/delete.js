0a402d1655bc32410d3359bc61bb363f643577c5
Jay Agrawal <69493224+JayAgra@users.noreply.github.com>
2022-04-14 17:44:44 -0700

    discord bot to fetch info

---
+++ discord/delete.js
@@ -0,0 +1,17 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
+const { clientId, token } = require('./config.json');
+    
+const rest = new REST({ version: '9' }).setToken(token);
+rest.get(Routes.applicationCommands(clientId))
+    .then(data => {
+        const promises = [];
+        for (const command of data) {
+            const deleteUrl = `${Routes.applicationCommands(clientId)}/${command.id}`;
+            promises.push(rest.delete(deleteUrl));
+            console.log(deleteUrl)
+            console.log('deleted')
+        }
+        return Promise.all(promises);
+    });
