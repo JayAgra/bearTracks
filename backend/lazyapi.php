5e9539342484d822ae60490cbdf7d6db9fca011a
Jayen Agrawal <69493224+JayAgra@users.noreply.github.com>
2022-09-13 12:21:50 -0700

    Add sentences to discord bot

--- backend/lazyapi.php
+++ backend/lazyapi.php
@@ -9,10 +9,6 @@ $url.= $_SERVER['REQUEST_URI'];
 $url_components = parse_url($url);
 parse_str($url_components['query'], $params);
 
-if ($params['teamnum']) {
-$season = $params['season'];
-$teamnum = $params['teamnum'];
-$event = $params['event'];
 class formDB extends SQLite3
 {
     function __construct()
@@ -20,6 +16,42 @@ class formDB extends SQLite3
         $this->open('data.db');
     }
 }
+$db = new formDB();
+
+if (isset($params['teamnum'])) {
+$season = $params['season'];
+$teamnum = $params['teamnum'];
+$event = $params['event'];
+$avgupma = $db->query('SELECT AVG(madeupper) FROM data WHERE teamnum='.$teamnum.' AND eventcode="'.$event.'"');
+while ($rowww = $avgupma->fetchArray()) {
+    $uppermaavg = $rowww[0];
+}
+$avgupmi = $db->query('SELECT AVG(missedupper) FROM data WHERE teamnum='.$teamnum.' AND eventcode="'.$event.'"');
+while ($rowwww = $avgupmi->fetchArray()) {
+    $uppermiavg = $rowwww[0];
+}
+$attemptedupper = $uppermaavg + $uppermiavg;
+$upperaccuracy = ($uppermaavg / $attemptedupper)*100;
+
+//lower
+
+$avgloma = $db->query('SELECT AVG(madelower) FROM data WHERE teamnum='.$teamnum.' AND eventcode="'.$event.'"');
+while ($rowwwww = $avgloma->fetchArray()) {
+    $lowermaavg = $rowwwww[0];
+}
+$avglomi = $db->query('SELECT AVG(missedlower) FROM data WHERE teamnum='.$teamnum.' AND eventcode="'.$event.'"');
+while ($rowwwwww = $avglomi->fetchArray()) {
+    $lowermiavg = $rowwwwww[0];
+}
+$attemptedlower = $lowermaavg + $lowermiavg;
+$loweraccuracy = ($lowermaavg / $attemptedlower)*100;
+
+$noclimbs = $db->query('SELECT barsdone, COUNT(barsdone) AS `value_occurrence` FROM data WHERE teamnum='.$teamnum.' AND eventcode="'.$event.'" GROUP BY barsdone ORDER BY  `value_occurrence` DESC LIMIT 1;');
+while ($rowwwwwwww = $noclimbs->fetchArray()) {
+ $mostclimbedbars = $rowwwwwwww[0];
+}
+//process sentence
+
 $db = new formDB();
 $avgupma = $db->query('SELECT AVG(madeupper) FROM data WHERE teamnum='.$teamnum.' AND eventcode="'.$event.'"');
 while ($rowww = $avgupma->fetchArray()) {
@@ -69,21 +101,24 @@ if ($upperaccuracy > 90) {
 }
 if ($loweraccuracy > 90) {
     $loweracccolor = '#32cd32';
-    $lowerrate = "and has an accurate lower shooter (".$loweraccuracy."%), ";
+    $lowerrate = "and has an accurate lower shooter (".$loweraccuracy."%). ";
 } else if ($loweraccuracy > 70) {
     $loweracccolor = 'yellow';
-    $lowerrate = "and has a somewhat accurate lower shooter (".$loweraccuracy."%), ";
+    $lowerrate = "and has a somewhat accurate lower shooter (".$loweraccuracy."%). ";
 } else if ($loweraccuracy > 50) {
     $loweracccolor = 'red';
-    $lowerrate = "and has an inaccurate lower shooter (".$loweraccuracy."%), ";
+    $lowerrate = "and has an inaccurate lower shooter (".$loweraccuracy."%). ";
 } else if ($loweraccuracy >30 ) {
     $loweracccolor = 'red';
-    $lowerrate = " has a very inaccurate lower shooter (".$loweraccuracy."%), ";
+    $lowerrate = " and has a very inaccurate lower shooter (".$loweraccuracy."%), ";
 } else if ($loweraccuracy = "NAN") {
     $loweracccolor = 'gray';
-    $lowerrate = " does not shoot in the lower hub, ";
+    $lowerrate = "and does not shoot in the lower hub. ";
 }
 
+$sentence =  ("Team ".$teamnum.$upperrate.$lowerrate."They shoot about ".$uppermaavg." upper and ".$lowermaavg." lower shots per game. They climb to the ".$mostclimbedbars." bar most often.");
+$querylink = "http://HOST.TLD/scout/query.php?season=".$season."&teamnum=".$teamnum."&event=".$event;
+//end sentence
 echo ('
 {
   "teamnum": "'.$teamnum.'",
@@ -92,9 +127,17 @@ echo ('
   "upperacc": "'.$upperaccuracy.'",
   "loweravg": "'.$lowermaavg.'",
   "loweracc": "'.$loweraccuracy.'",
-  "climbs": "'.$mostclimbedbars.'"
+  "climbs": "'.$mostclimbedbars.'",
+  "sentence": "'.$sentence.'",
+  "querylink": "'.$link.'"
 }');
-
-$db->close();
+} else if ($params['event']) {
+    $season = $params['season'];
+    $event = $params['event'];
+    $toppts = $db->query('SELECT * FROM data ORDER BY madeupper DESC LIMIT 6');
+    while($row=$toppts->fetchArray(SQLITE3_ASSOC)){
+        echo ('"shooter": "'.$row['teamnum'].'",');
+    }
 }
+$db->close();
 ?>
