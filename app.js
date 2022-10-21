7abc6453b0e06cb3e39db5cd9b3f7a2c9c3d3e38
Jayen Agrawal <69493224+JayAgra@users.noreply.github.com>
2022-10-21 16:56:55 -0700

    main form submissions now work, database fixed

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var querystring = require('querystring');
 var sqlite3 = require('sqlite3')
 var RequestIp = require('@supercharge/request-ip');
 const { mainhostname, myteam } = require('./config.json');
+const process = require('process');
+var time = new Date();
 
 //conenct to form database
 let db = new sqlite3.Database('./data.db', sqlite3.OPEN_READWRITE, (err) => {
@@ -49,41 +51,46 @@ http.createServer(function(request, response) {
               const ip = RequestIp.getClientIp(request)
               console.log('Submission on main form - sender IP "' + ip + '"');
               //insert data into table
-              db.run(`INSERT INTO data(eventcode, name, teamnum, teamnam, match, madeupper, missedupper, madelower, missedlower, barsatt, barsdone, autoattempt, shootauto, getauto, taxiauto, failauto, violate, human, performance, defend, teleop, driving, overall, matchlvl) VALUES ('${request.post.event}','${request.post.name}','${request.post.teamnum}','${request.post.teamnam}','${request.post.matchno}','${request.post.madeupper}','${request.post.missedupper}','${request.post.madelower}','${request.post.missedlower}','${request.post.barsatt}','${request.post.barsdone}','${request.post.autonATTSCOREFAIL}','${request.post.autonSHOOT}','${request.post.autonINTAKE}','${request.post.autonTAXI}','${request.post.autonATTSCOREFAIL}','no response','${request.post.humanplayer}','${request.post.consistent}','${request.post.defenseThought}','${request.post.drivingThought}','${request.post.overallThought}','${request.post.matchlvl}'`, function(err) {
+              db.run(`INSERT INTO data(eventcode, name, teamnum, teamnam, match, madeupper, missedupper, madelower, missedlower, barsatt, barsdone, autoattempt, shootauto, getauto, taxiauto, failauto, human, performance, defend, teleop, driving, overall, matchlvl, scoutip) 
+                      VALUES ("${request.post.event}","${request.post.name}","${request.post.teamnum}","${request.post.teamnam}","${request.post.matchno}","${request.post.madeupper}","${request.post.missedupper}","${request.post.madelower}","${request.post.missedlower}","${request.post.barsatt}","${request.post.barsdone}","${request.post.autonATTSCOREFAIL}","${request.post.autonSHOOT}","${request.post.autonINTAKE}","${request.post.autonTAXI}","${request.post.autonNOSCORE}","${request.post.humanplayer}","${request.post.consistent}","${request.post.defenseThought}","${request.post.teleopThought}","${request.post.drivingThought}","${request.post.overallThought}","${request.post.matchlvl}", "${ip}")`, function(err) {
                 if (err) {
                   response.writeHead(500, "INTERNAL SERVER ERROR", {'Content-Type': 'text/html'});
                   response.write('<!DOCTYPE html><body><h2>FATAL ERROR!</h2><h5>500 Internal Server Error</h5><h4>Try re-sending POST (reload)</h4><p>SENDER IP: "' + ip + '"</p><p>Submission ID: "main.' + err.message + '"</p></body>')
                   response.end();
                   //log error message, notify user of failure
-                  return console.log(err.message);
-                }
+                  console.log(err.message);
+                } else {
                 // get the last insert id
                 console.log('At ' + time + ', scout with IP "' + ip + '" submitted to main form. Submission ID: "main.' + this.lastID + '".')
+                //send success message
+                response.writeHead(200, "OK", {'Content-Type': 'text/html'});
+                response.write(`<!DOCTYPE html><head><meta http-equiv="refresh" content="3;url=http://${mainhostname}/scout/"><style>body {background-color: #121212;color: #fff;}</style></head><body><h1 style="color: green; text-align: center">Submitted!</h1><h3 style="text-align: center; color: lightgray;">Redirecting...</h3><p style="text-align: center">SENDER IP: "${ip}"</p><p style="text-align: center">Submission ID: "main.${this.lastID}"</p><p style="color: purple; text-align: center;">Submission Time: ${time}</p></body>`)
+                response.end();
+                }
               });
-              //send success message
-              response.writeHead(200, "OK", {'Content-Type': 'text/html'});
-              response.write(`<!DOCTYPE html><head><meta http-equiv="refresh" content="3;url=http://${mainhostname}/scout/"></head><body><h2>Done!</h2><p>SENDER IP: "${ip}"</p><p>Submission ID: "main.${this.lastID}"</p></body>`)
-              response.end();
             } else {
               const ip = RequestIp.getClientIp(request)
               console.log('Submission on pit form - sender IP "' + ip + '"');
               //insert data into table
               //send success message
               response.writeHead(200, "OK", {'Content-Type': 'text/html'});
-              response.write(`<!DOCTYPE html><head><meta http-equiv="refresh" content="3;url=http://${mainhostname}/scout/"></head><body><h2>Done!</h2><p>SENDER IP: "${ip}"</p><p>Submission ID: "main.${this.lastID}"</p></body>`)
+              response.write(`<!DOCTYPE html><head><meta http-equiv="refresh" content="3;url=http://${mainhostname}/scout/"><style>body {background-color: #121212;color: #fff;}</style></head><body><h2>Done!</h2><p>SENDER IP: "${ip}"</p><p>Submission ID: "main.${this.lastID}"</p></body>`)
               response.end();
+              return;
             }
           } else {
             console.log('no type specified')
             response.writeHead(400, "BAD REQUEST - MISSING CRITICAL DATA formType", {'Content-Type': 'text/plain'});
             response.write("400 - BAD REQUEST - MISSING CRITICAL DATA formType\nVALID VALUES OF formType: mainform, pitform")
             response.end();
+            return;
           }
       });
   } else {
       response.writeHead(400, "BAD REQUEST", {'Content-Type': 'text/plain'});
       response.write("400 - BAD REQUEST - MISSING ALL POST DATA")
       response.end();
+      return;
   }
 }).listen(766);
 console.log("server running on http://localhost:766/");
