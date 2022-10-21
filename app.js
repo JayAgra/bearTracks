f344f7292c1bcbd1e114a99ecc3d368079975d2b
Jayen Agrawal <69493224+JayAgra@users.noreply.github.com>
2022-10-21 11:06:53 -0700

    starting to add fill function

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 var http = require('http');
 var querystring = require('querystring');
 var sqlite3 = require('sqlite3')
-var RequestIp = require('@supercharge/request-ip')
+var RequestIp = require('@supercharge/request-ip');
+const { mainhostname, myteam } = require('./config.json');
 
 //conenct to form database
-let db = new sqlite3.Database('./data.db', (err) => {
+let db = new sqlite3.Database('./data.db', sqlite3.OPEN_READWRITE, (err) => {
   if (err) {
     console.error(err.message);
+    console.log("DATABASE CONNECTION FAILED")
   }
-  console.log('Connected to database');
+  console.log('Connected to database.');
 });
 
 
@@ -46,14 +48,29 @@ http.createServer(function(request, response) {
             if (request.post.formType === 'main') {
               const ip = RequestIp.getClientIp(request)
               console.log('Submission on main form - sender IP "' + ip + '"');
+              //insert data into table
+              db.run(`INSERT INTO data(eventcode, name, teamnum, teamnam, match, madeupper, missedupper, madelower, missedlower, barsatt, barsdone, autoattempt, shootauto, getauto, taxiauto, failauto, violate, human, performance, defend, teleop, driving, overall, matchlvl) VALUES ('${request.post.event}','${request.post.name}','${request.post.teamnum}','${request.post.teamnam}','${request.post.matchno}','${request.post.madeupper}','${request.post.missedupper}','${request.post.madelower}','${request.post.missedlower}','${request.post.barsatt}','${request.post.barsdone}','${request.post.autonATTSCOREFAIL}','${request.post.autonSHOOT}','${request.post.autonINTAKE}','${request.post.autonTAXI}','${request.post.autonATTSCOREFAIL}','no response','${request.post.humanplayer}','${request.post.consistent}','${request.post.defenseThought}','${request.post.drivingThought}','${request.post.overallThought}','${request.post.matchlvl}'`, function(err) {
+                if (err) {
+                  response.writeHead(500, "INTERNAL SERVER ERROR", {'Content-Type': 'text/html'});
+                  response.write('<!DOCTYPE html><body><h2>FATAL ERROR!</h2><h5>500 Internal Server Error</h5><h4>Try re-sending POST (reload)</h4><p>SENDER IP: "' + ip + '"</p><p>Submission ID: "main.' + err.message + '"</p></body>')
+                  response.end();
+                  //log error message, notify user of failure
+                  return console.log(err.message);
+                }
+                // get the last insert id
+                console.log('At ' + time + ', scout with IP "' + ip + '" submitted to main form. Submission ID: "main.' + this.lastID + '".')
+              });
+              //send success message
               response.writeHead(200, "OK", {'Content-Type': 'text/html'});
-              response.write('<!DOCTYPE html><head><meta http-equiv="refresh" content="3;url=http://example.com/scout/"></head><body><h2>Done!</h2><p>SENDER IP: "'+ip+'"</p></body>')
+              response.write(`<!DOCTYPE html><head><meta http-equiv="refresh" content="3;url=http://${mainhostname}/scout/"></head><body><h2>Done!</h2><p>SENDER IP: "${ip}"</p><p>Submission ID: "main.${this.lastID}"</p></body>`)
               response.end();
             } else {
               const ip = RequestIp.getClientIp(request)
               console.log('Submission on pit form - sender IP "' + ip + '"');
+              //insert data into table
+              //send success message
               response.writeHead(200, "OK", {'Content-Type': 'text/html'});
-              response.write('<!DOCTYPE html><head><meta http-equiv="refresh" content="3;url=http://example.com/scout/"></head><body><h2>Done!</h2><p>SENDER IP: "'+ip+'"</p></body>')
+              response.write(`<!DOCTYPE html><head><meta http-equiv="refresh" content="3;url=http://${mainhostname}/scout/"></head><body><h2>Done!</h2><p>SENDER IP: "${ip}"</p><p>Submission ID: "main.${this.lastID}"</p></body>`)
               response.end();
             }
           } else {
