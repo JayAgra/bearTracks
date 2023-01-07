996e8d79246f10ddf0558c69c360b42920d47f9e
Jayen Agrawal <69493224+JayAgra@users.noreply.github.com>
2023-01-07 14:15:56 -0800

    rename key

--- upload.js
+++ upload.js
@@ -48,7 +48,33 @@ const server = http.createServer((req, res) => {
             res.end('pit form submitted');
         } else if (formData.formType === 'main') {
             console.log("main recd from " +  req.socket.remoteAddress)
-            res.end('main form');
+            let db = new sqlite3.Database('data.db', sqlite3.OPEN_READWRITE, (err) => {
+                if (err) {
+                  console.error(err.message);
+                  res.end('pit form error! ' + err.message);
+                }
+                console.log('Connected to database');
+            });
+            let stmt = `INSERT INTO main (event, name, game1, game2, game3, game4, game5, game6, game7, game8, game9, game10, game11, game12, game13, game14, game15, game16, game17, game18, game19, game20, confidence, bqual, overall, scoutIP) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`;
+            let values = [formData.event, formData.name, formData.game1, formData.game2, formData.game3, formData.game4, formData.game5, formData.game6, formData.game7, formData.game8, formData.game9, formData.game10, formData.game11, formData.game12, formData.game13, formData.game14, formData.game15, formData.game16, formData.game17, formData.game18, formData.game19, formData.game20, formData.confidence, formData.bqual, formData.overall, req.socket.remoteAddress];
+            res.write("created stmt");
+            db.run(stmt, values, function(err) {
+                if (err) {
+                  console.error(err.message);
+                  res.end('pit form error! ' + err.message);
+                }
+                console.log(`row ${this.lastID} inserted`);
+                res.write("insterted data with row ID " + this.lastID + "\n");
+            });
+            db.close((err) => {
+                if (err) {
+                  console.error(err.message);
+                  res.end('pit form error! ' + err.message);
+                }
+                console.log('db closed');
+                res.write("finished writing to DB" + "\n");
+            });
+            res.end('main form submitted');
         } else {
           res.end('unknown form type');
           console.log("form type not known, got '" + formData.formType + "'")
