801b0bba1e57173685e68969d162cfd32ad81949
Jayen Agrawal <69493224+JayAgra@users.noreply.github.com>
2023-01-30 17:15:40 -0800

    attempt to redirect HTTP requests

--- serve.js
+++ serve.js
@@ -4,6 +4,7 @@ const express = require('express')
 const session  = require('express-session')
 const fs = require('fs');
 const https = require('https');
+const http = require('http');
 
 var app = express();
 
@@ -85,7 +86,7 @@ app.get('/logout', function(req, res) {
   if (req.session) {req.session.destroy(); res.redirect('/');} else {res.send("error!")}
 });
 
-//used for lets encrypt verification
+//use for lets encrypt verification
 app.get('/.well-known/acme-challenge/', function(req, res) {
   res.send("");
 });
@@ -296,11 +297,15 @@ function checkAuth(req, res, next) {
   res.redirect('/login');
 }
 
-if (fs.statSync("ssl/certificate.crt").size <= 100 || fs.statSync("ssl/privatekey.pem").size <= 100) {app.listen(80)} else {}
+if (fs.statSync("ssl/certificate.crt").size <= 100 || fs.statSync("ssl/privatekey.pem").size <= 100) {app.listen(80)} else {const httpRedirect = express(); httpRedirect.all('*', (req, res) => res.redirect(443, 'https://localhost')); const httpServer = http.createServer(httpRedirect); httpServer.listen(80, () => console.log(`HTTP server listening: http://localhost`));}
 
 //server created and ready for a request
 console.log('\x1b[35m', '[FORM PROCESSING] ' ,'\x1b[0m' + '\x1b[32m', '[INFO] ' ,'\x1b[0m' + "Ready!");
 
 //TODO: add a stats page that has info about each scout
+//on scout team or drive team or none
+//matches to scout if on scout team, next matches if on drive team, current index.html if on none
 //# of submitted forms, # of expected forms
 //schedule of matches to scout based on scout team role
+
+//TODO: Write docs
