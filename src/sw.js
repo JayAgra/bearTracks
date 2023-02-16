262d2aa38d1215e0ba26ab8374d93cd78384d9db
JayAgra <69493224+JayAgra@users.noreply.github.com>
2023-02-16 11:12:58 -0800

    work on service worker

--- src/sw.js
+++ src/sw.js
@@ -1,34 +1,54 @@
 //SERVICE WORKER
 
-self.addEventListener("install", (e) => {
-    console.log("SW - Installed");
-    const cacheName = "scouting-app-v1";
-    const contentToCache = [
-        '/2023_float.css',
-        '/2023_float.min.css',
-        '/fonts/Raleway-500.ttf',
-        '/fonts/Raleway-300.ttf'
-    ];
+console.log("SW Installed");
+const cacheName = "scouting-app-v1";
+const contentToCache = [
+    '/2023_float.css',
+    '/2023_float.min.css',
+    '/fonts/Raleway-500.ttf',
+    '/fonts/Raleway-300.ttf',
+    '/offline.html'
+];
+
+
+console.log('sw executed')
+self.addEventListener('install', function(e) {
     e.waitUntil(
-        (async () => {
-          const cache = await caches.open(cacheName);
-          console.log("SW - caching");
-          await cache.addAll(contentToCache);
-        })()
-      );
+        caches.open(cacheName).then(function(cache) {
+            return cache.addAll(contentToCache);
+        })
+    );
 });
 
-self.addEventListener("activate", (e) => {
-  e.waitUntil(
-    caches.keys().then((keyList) => {
-      return Promise.all(
-        keyList.map((key) => {
-          if (key === cacheName) {
-            return;
-          }
-          return caches.delete(key);
+console.log('service worker executed')
+self.addEventListener('install', function(e) {
+    e.waitUntil(
+        caches.open(cacheName).then(function(cache) {
+            return cache.addAll(contentToCache);
         })
-      );
-    })
-  );
+    );
+});
+
+self.addEventListener('activate', (evt) => {
+    evt.waitUntil(
+        caches.keys().then((keyList) => {
+            return Promise.all(keyList.map((key) => {
+                if (key !== cacheName) {
+                    return caches.delete(key);
+                }
+            }));
+        })
+    );
+    self.clients.claim();
+});
+
+/* Serve cache when online */
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+      fetch(event.request).catch(function(e) {
+        return caches.open(cacheName).then(function(cache) {
+          return cache.match(event.request,
+            {'ignoreSearch': true}).then(response => response);
+        });
+    }));
 });
