3bbd25d9d4770317f841036557753fa991fd5a57
JayAgra <69493224+JayAgra@users.noreply.github.com>
2023-02-16 15:13:05 -0800

    modify sw scope to be only offline.html

--- src/appinstall.js
+++ src/appinstall.js
@@ -2,7 +2,7 @@ const registerServiceWorker = async () => {
     if ("serviceWorker" in navigator) {
         try {
         const registration = await navigator.serviceWorker.register("/sw.js", {
-            scope: "/",
+            scope: "/offline.html",
         });
         if (registration.installing) {
             console.log("sw installing");
