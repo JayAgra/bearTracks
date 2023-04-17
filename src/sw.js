314319882df0f91b1937a605d51d173d121e440d
JayAgra <69493224+JayAgra@users.noreply.github.com>
2023-04-17 12:41:12 -0700

    actually just copy pasted MDN

--- src/sw.js
+++ src/sw.js
@@ -16,41 +16,24 @@ var filesToCache = [
     '/main'
 ];
 
-console.log("[SW] Executed");
-
 const addResourcesToCache = async (resources) => {
   const cache = await caches.open(cacheName);
   await cache.addAll(resources);
 };
 
-self.addEventListener("install", (event) => {
-  self.skipWaiting();
-  event.waitUntil(
-    addResourcesToCache(filesToCache)
-  );
-});
-
-const deleteCache = async (key) => {
-  await caches.delete(key);
+const putInCache = async (request, response) => {
+  const cache = await caches.open(cacheName);
+  await cache.put(request, response);
 };
-
-const deleteOldCaches = async () => {
-  const cacheKeepList = ["v2"];
-  const keyList = await caches.keys();
-  const cachesToDelete = keyList.filter((key) => !cacheKeepList.includes(key));
-  await Promise.all(cachesToDelete.map(deleteCache));
-};
-
-self.addEventListener("activate", (event) => {
-  event.waitUntil(deleteOldCaches());
-});
 
 const cacheFirst = async ({ request, preloadResponsePromise, fallbackUrl }) => {
+  // First try to get the resource from the cache
   const responseFromCache = await caches.match(request);
   if (responseFromCache) {
     return responseFromCache;
   }
 
+  // Next try to use (and cache) the preloaded response, if it's there
   const preloadResponse = await preloadResponsePromise;
   if (preloadResponse) {
     console.info("using preload response", preloadResponse);
@@ -58,9 +41,12 @@ const cacheFirst = async ({ request, preloadResponsePromise, fallbackUrl }) => {
     return preloadResponse;
   }
 
+  // Next try to get the resource from the network
   try {
     const responseFromNetwork = await fetch(request);
-
+    // response may be used only once
+    // we need to save clone to put one copy in cache
+    // and serve second one
     putInCache(request, responseFromNetwork.clone());
     return responseFromNetwork;
   } catch (error) {
@@ -68,13 +54,30 @@ const cacheFirst = async ({ request, preloadResponsePromise, fallbackUrl }) => {
     if (fallbackResponse) {
       return fallbackResponse;
     }
-
+    // when even the fallback response is not available,
+    // there is nothing we can do, but we must always
+    // return a Response object
     return new Response("Network error happened", {
       status: 408,
       headers: { "Content-Type": "text/plain" },
     });
   }
 };
+
+// Enable navigation preload
+const enableNavigationPreload = async () => {
+  if (self.registration.navigationPreload) {
+    await self.registration.navigationPreload.enable();
+  }
+};
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(enableNavigationPreload());
+});
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(addResourcesToCache(filesToCache));
+});
 
 self.addEventListener("fetch", (event) => {
   event.respondWith(
