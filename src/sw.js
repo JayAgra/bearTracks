eebd567fc16d21b7aaef005b376cd14d4e35bb5b
JayAgra <69493224+JayAgra@users.noreply.github.com>
2023-04-18 09:48:16 -0700

    Update sw.js

--- src/sw.js
+++ src/sw.js
@@ -1,35 +1,34 @@
 console.log("[SW] File Executed");
 
+const urlsToCache = ["/form.min.js","/float.min.css","/css/fonts/whatever-v8.woff","/assets/blackjack.js","/assets/spin.css","/assets/deal.png","/assets/hit.png","/assets/stand.png","/assets/wheel.png","/assets/yummy.mp3"];
+self.addEventListener("install", event => {
+console.log("[SW] Service worker installed");
+   event.waitUntil(
+        caches.open("pwa-assets").then(cache => {
+            return cache.addAll(urlsToCache);
+        })
+    );
+});
+
 self.addEventListener("activate", (event) => {
-  console.log("[SW] Activated");
-  event.waitUntil(self.registration.navigationPreload.enable());
+  console.log("[SW] Service worker activated");
 });
 
-self.addEventListener("install", function (event) {
-  console.log("[SW] Install Event");
-  event.waitUntil(
-    caches.open("scouting-2.0.0").then(function (cache) {
-      return cache.addAll([
-        "/form.min.js",
-        "/float.min.css",
-        "/css/fonts/whatever-v8.woff",
-        "/assets/blackjack.js",
-        "/assets/spin.css",
-        "/assets/deal.png",
-        "/assets/hit.png",
-        "/assets/stand.png",
-        "/assets/wheel.png",
-        "/assets/yummy.mp3",
-      ]);
-    })
-  );
-});
-
-self.addEventListener("fetch", function (event) {
-  console.log("[SW] Fetch Event");
+self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then(function (response) {
-      return response || fetch(event.request);
+    caches.match(event.request).then((cachedResponse) => {
+      const networkFetch = fetch(event.request)
+        .then((response) => {
+          const responseClone = response.clone();
+          caches.open(url.searchParams.get("name")).then((cache) => {
+            cache.put(event.request, responseClone);
+          });
+          return response;
+        })
+        .catch(function (reason) {
+          console.error("ServiceWorker fetch failed: ", reason);
+        });
+      return cachedResponse || networkFetch;
     })
   );
 });
