e9e6f1f43cc4507bef84b1d285e9a217946ac453
Jayen Agrawal <69493224+JayAgra@users.noreply.github.com>
2023-10-10 19:45:49 -0700

    Update teams.js

--- routes/api/teams.js
+++ routes/api/teams.js
@@ -1,12 +1,3 @@
-function addProperty(object, property, amount) {
-    if (object[property] !== undefined) {
-        object[property] += amount;
-    } else {
-        object[property] = 0;
-        addProperty(object, property, amount);
-    }
-}
-
 function teams(req, res, db, season) {
     if (req.params.event && req.params.type) {
         const stmt = `SELECT team, weight FROM main WHERE event=? AND season=? GROUP BY team`;
@@ -19,12 +10,6 @@ function teams(req, res, db, season) {
                 if (typeof dbQueryResult == "undefined") {
                     res.status(204).send("" + 0xcc1);
                 } else {
-                    var teams = {};
-                    var teamsCount = {};
-                    dbQueryResult.forEach((e) => {
-                        addProperty(teams, e.team, Number(e.weight.split(",")[Number(req.params.type)]));
-                        addProperty(teamsCount, e.team, 1);
-                    });
                     res.status(200).json(dbQueryResult);
                 }
             }
