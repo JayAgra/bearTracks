c680d7466fa8efd581f117609e5347d389696e2f
JayAgra <69493224+JayAgra@users.noreply.github.com>
2023-10-24 12:15:44 -0700

    email undef

--- routes/api/auth/create.js
+++ routes/api/auth/create.js
@@ -1,25 +1,33 @@
-var crypto = require('crypto');
+const qs = require("querystring");
+const crypto = require('crypto');
 
 function createAccount(req, res, authDb) {
-    let accountData = req.body;
-    /* email, password, full name, preferred name */
-    authDb.all("SELECT id FROM users WHERE email=?", [accountData.email], (err, result) => {
-        if (err) {
-            res.status(500).send("" + 0x1f42);
-        }
-        if (result.length === 0) {
-            const stmt = "INSERT INTO users (email, fullName, nickName, passHash, admin, accessOk, recentAttempts, lastLogin, score) VALUES (?, ?, ?, ?, 'false', 'false', 0, ?, 0)";
-            const values = [accountData.email, accountData.fullName, accountData.nickName, crypto.createHash('sha256').update(accountData.password).digest('hex'), String(Date.now())];
-            authDb.run(stmt, values, (err) => {
-                if (err) {
-                    res.status(500).send("" + 0x1f42);
-                } else {
-                    res.status(200).send("" + 0xc85);
-                }
-            })
-        } else {
-            res.status(409).send("" + 0x1991);
-        }
+    let body = "";
+
+    req.on("data", (chunk) => {
+        body += chunk.toString();
+    });
+
+    req.on("end", async () => {
+        let accountData = qs.parse(body);
+        authDb.all("SELECT id FROM users WHERE email=?", [accountData.email], (err, result) => {
+            if (err) {
+                res.status(500).send("" + 0x1f42);
+            }
+            if (result.length === 0) {
+                const stmt = "INSERT INTO users (email, fullName, nickName, passHash, admin, accessOk, recentAttempts, lastLogin, score) VALUES (?, ?, ?, ?, 'false', 'false', 0, ?, 0)";
+                const values = [accountData.email, accountData.fullName, accountData.nickName, crypto.createHash('sha256').update(accountData.password).digest('hex'), String(Date.now())];
+                authDb.run(stmt, values, (err) => {
+                    if (err) {
+                        res.status(500).send("" + 0x1f42);
+                    } else {
+                        res.status(200).send("" + 0xc85);
+                    }
+                })
+            } else {
+                res.status(409).send("" + 0x1991);
+            }
+        });
     });
 }
 
