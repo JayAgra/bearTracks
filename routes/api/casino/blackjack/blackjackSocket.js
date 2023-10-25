636d8c9b6a5897089d6996a987573de762c3560f
JayAgra <69493224+JayAgra@users.noreply.github.com>
2023-10-24 21:43:15 -0700

    fix noDeal button pos

--- routes/api/casino/blackjack/blackjackSocket.js
+++ routes/api/casino/blackjack/blackjackSocket.js
@@ -1,4 +1,4 @@
-const suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades'];
+const suits = ['h', 'd', 'c', 's'];
 const values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
 
 async function blackjackSocket(ws, req, transactions, authDb) {
