2e725cda1d665a059b0697a5ca0f17b73da0a1cd
JayAgra <69493224+JayAgra@users.noreply.github.com>
2023-10-24 17:08:32 -0700

    prevent server from crashing on bad pass

--- routes/api/auth/login.js
+++ routes/api/auth/login.js
@@ -12,11 +12,13 @@ function checkLogIn(req, res, authDb) {
         let loginData = qs.parse(body);
         authDb.get("SELECT id, fullName, passHash, accessOk, admin FROM users WHERE email=?", [loginData.email], (err, result) => {
             if (err) {
-                res.status(500).send("" + 0x1f42);
+                // res.status(500).send("" + 0x1f42);
+                res.redirect("/login?err=0");
             } else {
-                if (result.accessOk == "true") {
-                    if (!result) {
-                        res.status(409).send("" + 0x1992);
+                if (!result) {
+                    if (result.accessOk != "true") {
+                        // res.status(403).send("" + 0x1932 + " account not yet approved for access by an admin.");
+                        res.redirect("/login?err=2");
                     } else {
                         if (result.passHash === crypto.createHash('sha256').update(loginData.password).digest('hex')) {
                             const key = crypto.randomBytes(96).toString("hex");
@@ -31,7 +33,8 @@ function checkLogIn(req, res, authDb) {
                             ];
                             authDb.run(keyStmt, keyValues, (err) => {
                                 if (err) {
-                                    res.status(500).send("" + 0x1f42 + " internal server error (500)");
+                                    // res.status(500).send("" + 0x1f42 + " internal server error (500)");
+                                    res.redirect("/login?err=0");
                                 } else {
                                     res.cookie("key", key, {
                                         expire: Date.now() + 24 * 60 * 60 * 1000,
@@ -51,11 +54,13 @@ function checkLogIn(req, res, authDb) {
                                 }
                             });
                         } else {
-                            res.status(409).send("" + 0x1992 + " bad username/password");
+                            // res.status(409).send("" + 0x1992 + " bad username/password");
+                            res.redirect("/login?err=1");
                         }
                     }
                 } else {
-                    res.status(403).send("" + 0x1932 + " account not yet approved for access by an admin.");
+                    // res.status(409).send("" + 0x1992 + " bad username/password");
+                    res.redirect("/login?err=1");
                 }
             }
         });
---
+++ src/js/login_client.js
@@ -0,0 +1,20 @@
+function displayErrors() {
+    const urlParams = new URLSearchParams(window.location.search);
+    const error = urlParams.get("err");
+    const errorEl = document.getElementById("error");
+    if (error) {
+        switch (error) {
+            case "0":
+                errorEl.innerText = "500 internal server error";
+                errorEl.style.display = "unset";
+            case "1":
+                errorEl.innerText = "bad email/password";
+                errorEl.style.display = "unset";
+            case "2":
+                errorEl.innerText = "account not yet approved by an admin";
+                errorEl.style.display = "unset";
+            case undefined: default:
+                return;
+        }
+    }
+}
