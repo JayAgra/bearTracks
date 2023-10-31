ab5de359a4c26935f4bbc9b671e18cd8c10bc7b6
JayAgra <69493224+JayAgra@users.noreply.github.com>
2023-10-31 16:54:57 -0700

    fix endpoint

--- routes/api/manage/teams/list.ts
+++ routes/api/manage/teams/list.ts
@@ -3,8 +3,8 @@ import * as sqlite3 from "sqlite3";
 
 export async function listTeams(req: express.Request, res: express.Response, authDb: sqlite3.Database) {
     if (req.user.admin === "true") {
-        const stmt: string = `SELECT * FROM accessKeys WHERE team=?`;
-        authDb.all(stmt, [req.user.teamAdmin], (err: any, dbQueryResult: Array<Object> | undefined) => {
+        const stmt: string = `SELECT * FROM accessKeys`;
+        authDb.all(stmt, (err: any, dbQueryResult: Array<Object> | undefined) => {
             if (err) {
                 return res.status(500).send("" + 0x1f41);
             } else {
