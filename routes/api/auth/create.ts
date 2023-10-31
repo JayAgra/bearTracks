f2b960e0675dc86971f878e536cbe584a38387aa
JayAgra <69493224+JayAgra@users.noreply.github.com>
2023-10-31 16:46:56 -0700

    async hash

--- routes/api/auth/create.ts
+++ routes/api/auth/create.ts
@@ -38,37 +38,37 @@ export async function createAccount(req: express.Request, res: express.Response,
             if (result.length === 0) {
                 if (accountData.password.length >= 8) {
                     var targetTeam: number = 0;
-                    authDb.get("SELECT team FROM accessKeys WHERE key=?", [Number(accountData.access)], (err: any, result: {"team": number} | undefined) => {
+                    authDb.get("SELECT team FROM accessKeys WHERE key=?", [Number(accountData.access)], async (err: any, result: {"team": number} | undefined) => {
                         if (err || !result) {
                             return res.redirect("/create?err=3");
                         } else {
                             targetTeam = result.team;
+                            if (targetTeam !== 0) {
+                                const salt = randomBytes(32).toString("hex");
+                                const stmt = "INSERT INTO users (email, fullName, nickName, team, passHash, salt, admin, teamAdmin, accessOk, recentAttempts, lastLogin, score) VALUES (?, ?, ?, ?, ?, ?, 'false', 0, 'false', 0, ?, 0)";
+                                const values = [
+                                    escapeHTML(accountData.email),
+                                    escapeHTML(accountData.fullName),
+                                    escapeHTML(accountData.nickName),
+                                    targetTeam,
+                                    await Bun.password.hash(accountData.password + salt),
+                                    salt,
+                                    String(Date.now()),
+                                ];
+                                authDb.run(stmt, values, (err: any) => {
+                                    if (err) {
+                                        // res.status(500).send("" + 0x1f42);
+                                        return res.redirect("/create?err=0");
+                                    } else {
+                                        // res.status(200).send("" + 0xc85);
+                                        return res.redirect("/login");
+                                    }
+                                });
+                            } else {
+                                return res.redirect("/create?err=3");
+                            }
                         }
                     });
-                    if (targetTeam !== 0) {
-                        const salt = randomBytes(32).toString("hex");
-                        const stmt = "INSERT INTO users (email, fullName, nickName, team, passHash, salt, admin, teamAdmin, accessOk, recentAttempts, lastLogin, score) VALUES (?, ?, ?, ?, ?, ?, 'false', 0, 'false', 0, ?, 0)";
-                        const values = [
-                            escapeHTML(accountData.email),
-                            escapeHTML(accountData.fullName),
-                            escapeHTML(accountData.nickName),
-                            targetTeam,
-                            Bun.password.hash(accountData.password + salt),
-                            salt,
-                            String(Date.now()),
-                        ];
-                        authDb.run(stmt, values, (err: any) => {
-                            if (err) {
-                                // res.status(500).send("" + 0x1f42);
-                                return res.redirect("/create?err=0");
-                            } else {
-                                // res.status(200).send("" + 0xc85);
-                                return res.redirect("/login");
-                            }
-                        });
-                    } else {
-                        return res.redirect("/create?err=3");
-                    }
                 } else {
                     return res.redirect("/create?err=2");
                 }
