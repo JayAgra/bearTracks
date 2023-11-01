06c296bcffc82e9444e02507e46f541538d79fc7
JayAgra <69493224+JayAgra@users.noreply.github.com>
2023-10-31 19:57:15 -0700

    fix something idk

--- routes/api/manage/myTeam/scouts/access.ts
+++ routes/api/manage/myTeam/scouts/access.ts
@@ -5,14 +5,14 @@ export async function manageTeamUser(req: express.Request, res: express.Response
     if (req.user.teamAdmin !== 0 || req.user.admin === "true") {
         const team = req.user.teamAdmin === 0 ? "*" : req.user.teamAdmin;
         if (req.params.accessOk === "false") {
-            authDb.run("UPDATE users SET teamAdmin=0 WHERE id=? AND team=?", [req.params.id, team]);
+            authDb.run("UPDATE users SET teamAdmin=NULL WHERE id=? AND team=?", [req.params.id, team]);
             authDb.run("DELETE FROM keys WHERE userId=? AND team=?", [req.params.id, team]);
         }
         authDb.run("UPDATE users SET accessOk=? WHERE id=? AND team=?", [req.params.accessOk, req.params.id, team], (err: any,) => {
             if (err) {
                 return res.status(500).send("" + 0x1f42);
             } else {
-                res.status(200).send("" + 0xc86);
+                return res.status(200).send("" + 0xc86);
             }
         });
     } else {
