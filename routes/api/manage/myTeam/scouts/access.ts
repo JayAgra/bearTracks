a361d75e74bb4247998581d49feb622d7cf22a78
JayAgra <69493224+JayAgra@users.noreply.github.com>
2023-10-31 19:53:04 -0700

    also destory session

--- routes/api/manage/myTeam/scouts/access.ts
+++ routes/api/manage/myTeam/scouts/access.ts
@@ -4,6 +4,10 @@ import * as sqlite3 from "sqlite3";
 export async function manageTeamUser(req: express.Request, res: express.Response, authDb: sqlite3.Database) {
     if (req.user.teamAdmin !== 0 || req.user.admin === "true") {
         const team = req.user.teamAdmin === 0 ? "*" : req.user.teamAdmin;
+        if (req.params.accessOk === "false") {
+            authDb.run("UPDATE users SET teamAdmin=0 WHERE id=? AND team=?", [req.params.accessOk, req.params.id, team]);
+            authDb.run("DELETE FROM keys WHERE userId=? AND team=?", [req.params.id, team]);
+        }
         authDb.run("UPDATE users SET accessOk=? WHERE id=? AND team=?", [req.params.accessOk, req.params.id, team], (err: any,) => {
             if (err) {
                 return res.status(500).send("" + 0x1f42);
