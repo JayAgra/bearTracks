4c9518f67b37f04642985f08406fc3a936b6af1e
JayAgra <69493224+JayAgra@users.noreply.github.com>
2023-10-31 17:22:53 -0700

    0

--- routes/api/manage/teams/list.ts
+++ routes/api/manage/teams/list.ts
@@ -4,7 +4,7 @@ import * as sqlite3 from "sqlite3";
 export async function listTeams(req: express.Request, res: express.Response, authDb: sqlite3.Database) {
     if (req.user.admin == "true") {
         authDb.all("SELECT * FROM accessKeys", (err: any, dbQueryResult: Array<Object> | undefined) => {
-            if (err) {
+            if (err || typeof dbQueryResult == "undefined") {
                 return res.status(500).send("" + 0x1f41);
             } else {
                 return res.status(200).json(dbQueryResult);
