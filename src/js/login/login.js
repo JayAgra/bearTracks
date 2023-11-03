1393872e66bba5f6a8551b78a2c7db90c8fbba27
JayAgra <69493224+JayAgra@users.noreply.github.com>
2023-11-03 09:03:21 -0700

    fix error scripts

--- src/js/login/login.js
+++ src/js/login/login.js
@@ -1,5 +1,5 @@
 import { _dErr } from "../_modules/dErr/dErr.min.js";
 function displayLoginErrors() {
-    _dErr("500 internal server error", "bad email/password", "account not yet approved by an admin", "");
+    _dErr(["500 internal server error", "bad email/password", "account not yet approved by an admin", ""]);
 }
 window.displayLoginErrors = displayLoginErrors;
--- src/js/login/login.ts
+++ src/js/login/login.ts
@@ -1,7 +1,7 @@
 import { _dErr } from "../_modules/dErr/dErr.min.js";
 
 function displayLoginErrors() {
-    _dErr("500 internal server error", "bad email/password", "account not yet approved by an admin", "");
+    _dErr(["500 internal server error", "bad email/password", "account not yet approved by an admin", ""]);
 }
 
 (window as any).displayLoginErrors = displayLoginErrors;
