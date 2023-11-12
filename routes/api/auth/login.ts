6224898a9d67165358c12b6e7972e0f5da54ee0c
JayAgra <69493224+JayAgra@users.noreply.github.com>
2023-11-11 16:18:00 -0800

    oiakjhwefgsvdkcuyihbvaegwDHSUefiw

--- routes/api/auth/login.ts
+++ routes/api/auth/login.ts
@@ -22,7 +22,7 @@ type authData = {
 
 export async function checkLogIn(req: express.Request, res: express.Response, authDb: sqlite3.Database) {
     let loginData: loginDataForm = req.body as unknown as loginDataForm;
-    console.log(loginData);
+    console.log(req.body);
     authDb.get("SELECT id, username, fullName, team, passHash, salt, accessOk, admin, teamAdmin FROM users WHERE username=?", [loginData.username], async (err: any, result: authData | undefined) => {
         if (err) {
             // res.status(500).json({ "status": 0x1f42 });
