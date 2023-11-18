722f6df5bd0395a78d2c51a0500e1a314c14547f
JayAgra <69493224+JayAgra@users.noreply.github.com>
2023-11-17 22:43:10 -0800

    delete some old crap

--- old/src/escape.ts
+++
@@ -1,8 +0,0 @@
-export function escapeHTML(htmlStr: string): string {
-    return String(htmlStr)
-        .replace(/&/g, "&amp;")
-        .replace(/</g, "&lt;")
-        .replace(/>/g, "&gt;")
-        .replace(/"/g, "&quot;")
-        .replace(/'/g, "&#39;");
-}
