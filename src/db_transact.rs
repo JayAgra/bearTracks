e63cd5895b86a73702d46a45e174a85fa082659e
JayAgra <69493224+JayAgra@users.noreply.github.com>
2023-11-17 22:04:56 -0800

    transactions endpoint

---
+++ src/db_transact.rs
@@ -0,0 +1,56 @@
+use actix_web::{error, web, Error};
+use rusqlite::Statement;
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Transact {
+    pub id: i32,
+    pub user_id: String,
+    pub trans_type: i32,
+    pub amount: i32,
+    pub time: String
+}
+
+
+pub type Pool = r2d2::Pool<r2d2_sqlite::SqliteConnectionManager>;
+pub type Connection = r2d2::PooledConnection<r2d2_sqlite::SqliteConnectionManager>;
+type TransactQuery = Result<Vec<Transact>, rusqlite::Error>;
+
+pub enum TransactData {
+    GetUserTransactions,
+}
+
+pub async fn execute(pool: &Pool, query: TransactData) -> Result<Vec<Transact>, Error> {
+    let pool = pool.clone();
+
+    let conn = web::block(move || pool.get())
+        .await?
+        .map_err(error::ErrorInternalServerError)?;
+
+    web::block(move || {
+        match query {
+            TransactData::GetUserTransactions => get_user_transact(conn),
+        }
+    })
+    .await?
+    .map_err(error::ErrorInternalServerError)
+}
+
+fn get_user_transact(conn: Connection) -> TransactQuery {
+    let stmt = conn.prepare("SELECT * FROM transactions WHERE user_id=1")?;
+    get_transact_rows(stmt)
+}
+
+fn get_transact_rows(mut statement: Statement) -> TransactQuery {
+    statement
+        .query_map([], |row| {
+            Ok(Transact {
+                id: row.get(0)?,
+                user_id: row.get(1)?,
+                trans_type: row.get(2)?,
+                amount: row.get(3)?,
+                time: row.get(4)?,
+            })
+        })
+        .and_then(Iterator::collect)
+}
--- src/main.rs
+++ src/main.rs
@@ -3,32 +3,40 @@ use std::io;
 use actix_web::{middleware, web, App, Error as AWError, HttpResponse, HttpServer};
 use r2d2_sqlite::{self, SqliteConnectionManager};
 
-mod main_db;
+mod db_main;
+mod db_transact;
 
-async fn get_detailed(db: web::Data<main_db::Pool>) -> Result<HttpResponse, AWError> {
+async fn data_get_detailed(db: web::Data<db_main::Pool>) -> Result<HttpResponse, AWError> {
     let result = vec![
-        main_db::execute(&db, main_db::MainData::GetDataDetailed).await?,
+        db_main::execute(&db, db_main::MainData::GetDataDetailed).await?,
     ];
     Ok(HttpResponse::Ok().json(result))
 }
 
-async fn get_main_brief_team(db: web::Data<main_db::Pool>) -> Result<HttpResponse, AWError> {
+async fn data_get_main_brief_team(db: web::Data<db_main::Pool>) -> Result<HttpResponse, AWError> {
     let result = vec![
-        main_db::execute_get_brief(&db, main_db::MainBrief::BriefTeam).await?,
+        db_main::execute_get_brief(&db, db_main::MainBrief::BriefTeam).await?,
     ];
     Ok(HttpResponse::Ok().json(result))
 }
 
-async fn get_main_brief_match(db: web::Data<main_db::Pool>) -> Result<HttpResponse, AWError> {
+async fn data_get_main_brief_match(db: web::Data<db_main::Pool>) -> Result<HttpResponse, AWError> {
     let result = vec![
-        main_db::execute_get_brief(&db, main_db::MainBrief::BriefMatch).await?,
+        db_main::execute_get_brief(&db, db_main::MainBrief::BriefMatch).await?,
     ];
     Ok(HttpResponse::Ok().json(result))
 }
 
-async fn get_main_brief_event(db: web::Data<main_db::Pool>) -> Result<HttpResponse, AWError> {
+async fn data_get_main_brief_event(db: web::Data<db_main::Pool>) -> Result<HttpResponse, AWError> {
     let result = vec![
-        main_db::execute_get_brief(&db, main_db::MainBrief::BriefEvent).await?,
+        db_main::execute_get_brief(&db, db_main::MainBrief::BriefEvent).await?,
+    ];
+    Ok(HttpResponse::Ok().json(result))
+}
+
+async fn misc_transact_get_me(db: web::Data<db_transact::Pool>) -> Result<HttpResponse, AWError> {
+    let result = vec![
+        db_transact::execute(&db, db_transact::TransactData::GetUserTransactions).await?,
     ];
     Ok(HttpResponse::Ok().json(result))
 }
@@ -38,20 +46,31 @@ async fn main() -> io::Result<()> {
     env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
 
     let main_db_manager = SqliteConnectionManager::file("data.db");
-    let main_db_pool = main_db::Pool::new(main_db_manager).unwrap();
+    let main_db_pool = db_main::Pool::new(main_db_manager).unwrap();
+
+    let auth_db_manager = SqliteConnectionManager::file("data_auth.db");
+    let auth_db_pool = db_main::Pool::new(auth_db_manager).unwrap();
+
+    let trans_db_manager = SqliteConnectionManager::file("data_transact.db");
+    let trans_db_pool = db_transact::Pool::new(trans_db_manager).unwrap();
 
     log::info!("starting bearTracks on port 8000");
 
     HttpServer::new(move || {
         App::new()
             .app_data(web::Data::new(main_db_pool.clone()))
+            .app_data(web::Data::new(auth_db_pool.clone()))
+            .app_data(web::Data::new(trans_db_pool.clone()))
             .wrap(middleware::Logger::default())
-            /* endpoints */
-            .service(web::resource("/api/v1/data/detail").route(web::get().to(get_detailed)))
-            .service(web::resource("/api/v1/data/brief/team").route(web::get().to(get_main_brief_team)))
-            .service(web::resource("/api/v1/data/brief/match").route(web::get().to(get_main_brief_match)))
-            .service(web::resource("/api/v1/data/brief/event").route(web::get().to(get_main_brief_event)))
-            /* end endpoints */
+            /* auth endpoints */
+            /* data endpoints */
+                .service(web::resource("/api/v1/data/detail").route(web::get().to(data_get_detailed)))
+                .service(web::resource("/api/v1/data/brief/team").route(web::get().to(data_get_main_brief_team)))
+                .service(web::resource("/api/v1/data/brief/match").route(web::get().to(data_get_main_brief_match)))
+                .service(web::resource("/api/v1/data/brief/event").route(web::get().to(data_get_main_brief_event)))
+            /* user endpoints */
+            /* misc endpoints */
+                .service(web::resource("/api/v1/transact/me").route(web::get().to(misc_transact_get_me)))
     })
     .bind(("127.0.0.1", 8000))?
     .workers(2)
