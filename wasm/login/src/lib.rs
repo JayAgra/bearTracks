1a9c1b2fd3b64b5b56c391486f0cc94af347f4dd
Jayen Agrawal <69493224+JayAgra@users.noreply.github.com>
2024-02-19 22:25:49 -0800

    Capitalization chaos (#41)
    autocap no

--- wasm/login/src/lib.rs
+++ wasm/login/src/lib.rs
@@ -38,8 +38,8 @@ impl App {
                                     <h2 class="unValue">{ "please log in" }</h2>
                                 </div>
                                 <h3 id="error" style="color: var(--cancelColor);"></h3>
-                                <input id="username_input" type="text" autocomplete="username" placeholder="username" class="ci" name="username"/>
-                                <input id="inputPassword" type="password" autocomplete="current-password" placeholder="password" name="password" class="ci" style="display: none;"/>
+                                <input id="username_input" type="text" autocomplete="username" placeholder="username" autocapitalize="none" class="ci" name="username"/>
+                                <input id="inputPassword" type="password" autocomplete="current-password" placeholder="password" autocapitalize="none" name="password" class="ci" style="display: none;"/>
                                 <div class="buttonCont">
                                     <button
                                         id="pw"
