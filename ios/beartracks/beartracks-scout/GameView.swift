0d77aec1e3c3144e25aee1e2a0ed32e8a5d543a0
JayAgra <69493224+JayAgra@users.noreply.github.com>
2024-02-27 00:02:18 -0800

    completely rework scouting interface
    pill shaped goofy gear shifter type interface

--- ios/beartracks/beartracks-scout/GameView.swift
+++ ios/beartracks/beartracks-scout/GameView.swift
@@ -6,122 +6,189 @@
 //
 
 import SwiftUI
+import AudioToolbox
 
 struct GameView: View {
     @EnvironmentObject var controller: ScoutingController
     @State private var holdLengths: (TimeInterval, TimeInterval, TimeInterval) = (0.0, 0.0, 0.0)
-    @State private var timers: (Timer?, Timer?, Timer?)
-    @State private var pressStarted: (Bool, Bool, Bool) = (false, false, false)
+    @State private var timer: Timer?
+    @State private var actionState: ActionState = .neutral
+    @State private var ballOffset: CGSize = .zero
+    
+    enum ActionState {
+        case neutral, intake, travel, outtake
+    }
     
     var body: some View {
-        VStack {
-            NavigationStack {
+        NavigationStack {
+            if controller.getTeamNumber() != "--" && controller.getMatchNumber() != "--" {
                 VStack {
-                    if controller.getTeamNumber() != "--" && controller.getMatchNumber() != "--" {
-                        HStack {
-                            VStack {
-                                Text(String(format: "%.1f", holdLengths.0))
-                                    .font(.largeTitle)
-                                Button("intake", systemImage: "tray.and.arrow.down") {}
-                                .modifier(PressModifier(onPress: {
-                                    if !pressStarted.0 {
-                                        pressStarted.0 = true
-                                        self.timers.0 = Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true) { _ in
-                                            self.holdLengths.0 += 0.1
-                                            controller.times[0] = self.holdLengths.0
+                    GeometryReader { geometry in
+                        VStack {
+                            Text("\(String(format: "%.1f", holdLengths.0)) \(String(format: "%.1f", holdLengths.1)) \(String(format: "%.1f", holdLengths.2))")
+                                .frame(maxWidth: .infinity, alignment: .center)
+                                .padding(.top)
+                            Spacer()
+                            ZStack {
+                                HStack {
+                                    Spacer()
+                                    Capsule()
+                                        .fill(Color.init(red: 0.1, green: 0.1, blue: 0.1))
+                                        .frame(width: geometry.size.width * 0.8, height: 60)
+                                    Spacer()
+                                }
+                                Circle()
+                                    .fill(getBallColor(position: actionState, height: geometry.size.height))
+                                    .overlay (
+                                        getUIImage(position: actionState, height: geometry.size.height)
+                                            .colorMultiply(Color.init(red: 0.1, green: 0.1, blue: 0.1))
+                                            .bold()
+                                    )
+                                    .frame(width: 50, height: 50)
+                                    .offset(x: ballOffset.width, y: 0)
+                                    .gesture(
+                                        DragGesture()
+                                            .onChanged { value in
+                                                self.updateBallOffset(dragValue: value, totalWidth: geometry.size.width * 0.8)
+                                                self.updateTogglePosition(totalWidth: geometry.size.width * 0.8, height: geometry.size.height)
+                                            }
+                                            .onEnded { _ in
+                                                withAnimation {
+                                                    if abs(self.ballOffset.height) > geometry.size.height * 0.25 {
+                                                        UINotificationFeedbackGenerator().notificationOccurred(.success)
+                                                    }
+                                                    self.ballOffset = .zero
+                                                    self.actionState = .neutral
+                                                }
+                                            }
+                                    )
+                                    .modifier(PressModifier(onPress: { self.actionState = .travel }, onRelease: {
+                                        if self.ballOffset.height > geometry.size.height * 0.2 {
+                                            let boomThing = UIImpactFeedbackGenerator(style: .medium)
+                                            boomThing.prepare(); boomThing.impactOccurred();
+                                            self.holdLengths = (0, 0, 0)
+                                            controller.clearAmplifier()
+                                            self.ballOffset = .zero
+                                        } else if self.ballOffset.height < geometry.size.height * -0.2 {
+                                            UINotificationFeedbackGenerator().notificationOccurred(.success)
+                                            self.holdLengths = (0, 0, 0)
+                                            controller.clearSpeaker()
+                                            self.ballOffset = .zero
+                                        } else {
+                                            self.actionState = .neutral
                                         }
-                                        UINotificationFeedbackGenerator().notificationOccurred(.success)
+                                    }))
+                            }
+                            .padding(.bottom)
+                            .onChange(of: actionState) { value in
+                                self.timer?.invalidate()
+                                self.timer = Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true) { _ in
+                                    if value == .intake {
+                                        self.holdLengths.0 += 0.1
+                                        controller.times[0] = self.holdLengths.0
+                                    } else if value == .travel {
+                                        self.holdLengths.1 += 0.1
+                                        controller.times[1] = self.holdLengths.1
+                                    } else if value == .outtake {
+                                        self.holdLengths.2 += 0.1
+                                        controller.times[2] = self.holdLengths.2
                                     }
-                                }, onRelease: {
-                                    self.timers.0?.invalidate()
-                                    self.pressStarted.0 = false
-                                    UINotificationFeedbackGenerator().notificationOccurred(.success)
-                                }))
-                                .font(.largeTitle)
-                                .labelStyle(.iconOnly)
-                                .buttonStyle(.bordered)
+                                }
+                                UIImpactFeedbackGenerator(style: .rigid).impactOccurred()
+                            }
+                            Spacer()
+                            Button("go to endgame") {
+                                controller.advanceToTab(tab: .end)
                             }
                             .padding()
-                            VStack {
-                                Text(String(format: "%.1f", holdLengths.1))
-                                    .font(.largeTitle)
-                                Button("move", systemImage: "arrow.up.and.down.and.arrow.left.and.right") {}
-                                .modifier(PressModifier(onPress: {
-                                    if !pressStarted.1 {
-                                        pressStarted.1 = true
-                                        self.timers.1 = Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true) { _ in
-                                            self.holdLengths.1 += 0.1
-                                            controller.times[1] = self.holdLengths.1
-                                        }
-                                        UINotificationFeedbackGenerator().notificationOccurred(.success)
-                                    }
-                                }, onRelease: {
-                                    self.timers.1?.invalidate()
-                                    self.pressStarted.1 = false
-                                    UINotificationFeedbackGenerator().notificationOccurred(.success)
-                                }))
-                                .font(.largeTitle)
-                                .labelStyle(.iconOnly)
-                                .buttonStyle(.bordered)
-                            }
-                            .padding()
-                            VStack {
-                                Text(String(format: "%.1f", holdLengths.2))
-                                    .font(.largeTitle)
-                                Button("outtake", systemImage: "tray.and.arrow.up") {}
-                                .modifier(PressModifier(onPress: {
-                                    if !pressStarted.2 {
-                                        pressStarted.2 = true
-                                        self.timers.2 = Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true) { _ in
-                                            self.holdLengths.2 += 0.1
-                                            controller.times[2] = self.holdLengths.2
-                                        }
-                                        UINotificationFeedbackGenerator().notificationOccurred(.success)
-                                    }
-                                }, onRelease: {
-                                    self.timers.2?.invalidate()
-                                    self.pressStarted.2 = false
-                                    UINotificationFeedbackGenerator().notificationOccurred(.success)
-                                }))
-                                .font(.largeTitle)
-                                .labelStyle(.iconOnly)
-                                .buttonStyle(.bordered)
-                            }
-                            .padding()
-                        }
-                        .padding(.bottom)
-                        HStack {
-                            Button("speaker", systemImage: "speaker.wave.3") {
-                                controller.clearSpeaker()
-                                self.holdLengths = (0, 0, 0)
-                            }
-                            .font(.title)
                             .buttonStyle(.bordered)
-                            .foregroundStyle(Color.green)
+                            Text("match \(controller.getMatchNumber()) • team \(controller.getTeamNumber())")
+                                .frame(maxWidth: .infinity, alignment: .center)
+                                .padding(.bottom)
                         }
-                        HStack {
-                            Button("amplifier", systemImage: "speaker.plus") {
-                                controller.clearAmplifier()
-                                self.holdLengths = (0, 0, 0)
-                            }
-                            .font(.title)
-                            .buttonStyle(.bordered)
-                            .foregroundStyle(Color.green)
-                        }
-                        Button("go to endgame") {
-                            controller.advanceToTab(tab: .end)
-                        }
-                        .padding()
-                        .buttonStyle(.bordered)
-                        Text("match \(controller.getMatchNumber()) • team \(controller.getTeamNumber())")
-                            .frame(maxWidth: .infinity, alignment: .center)
-                            .padding(.top)
-                    } else {
-                        Text("Please select a match number and event code on the start tab.")
-                            .padding()
+                        .frame(width: geometry.size.width, height: geometry.size.height)
                     }
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
                 }
                 .navigationTitle("Match Scouting")
+            } else {
+                Text("Please select a match number and event code on the start tab.")
+                    .padding()
+                    .navigationTitle("Match Scouting")
+            }
+        }
+    }
+    
+    private func updateTogglePosition(totalWidth: CGFloat, height: CGFloat) {
+        let position: CGFloat
+        if ballOffset.width > 0 {
+            position = ((ballOffset.width + 25) + (totalWidth * 0.5)) / totalWidth
+        } else {
+            position = ((ballOffset.width - 25) + (totalWidth * 0.5)) / totalWidth
+        }
+        
+        switch position {
+        case ..<0.25:
+            self.actionState = .intake
+        case 0.25..<0.75:
+            self.actionState = .travel
+        default:
+            self.actionState = .outtake
+        }
+        
+        if abs(self.ballOffset.height) > height * 0.2 {
+            // UIImpactFeedbackGenerator(style: .light).impactOccurred()
+            AudioServicesPlaySystemSound(kSystemSoundID_Vibrate)
+        }
+    }
+
+    private func updateBallOffset(dragValue: DragGesture.Value, totalWidth: CGFloat) {
+        let newOffset = CGSize(
+            width: min(max(dragValue.translation.width, (totalWidth * -0.5) + 30), (totalWidth * 0.5) - 30),
+            height: dragValue.translation.height
+        )
+
+        self.ballOffset = newOffset
+    }
+    
+    private func getUIImage(position: ActionState, height: CGFloat) -> Image {
+        if abs(self.ballOffset.height) > height * 0.2 {
+            if self.ballOffset.height < 0 {
+                return Image(systemName: "speaker.wave.2")
+            } else {
+                return Image(systemName: "speaker.plus")
+            }
+        } else {
+            switch position {
+            case .neutral:
+                return Image(systemName: "arrow.left.and.right")
+            case .intake:
+                return Image(systemName: "chevron.down")
+            case .travel:
+                return Image(systemName: "arrow.up.and.down.and.arrow.left.and.right")
+            case .outtake:
+                return Image(systemName: "chevron.up")
+            }
+        }
+    }
+    
+    private func getBallColor(position: ActionState, height: CGFloat) -> Color {
+        if abs(self.ballOffset.height) > height * 0.2 {
+            if self.ballOffset.height < 0 {
+                return Color.init(red: 184/255, green: 187/255, blue: 38/255)
+            } else {
+                return Color.init(red: 250/255, green: 189/255, blue: 47/255)
+            }
+        } else {
+            switch position {
+            case .neutral:
+                return Color.init(red: 251/255, green: 241/255, blue: 199/255)
+            case .intake:
+                return Color.init(red: 69/255, green: 133/255, blue: 136/255)
+            case .travel:
+                return Color.init(red: 177/255, green: 98/255, blue: 134/255)
+            case .outtake:
+                return Color.init(red: 104/255, green: 157/255, blue: 106/255)
             }
         }
     }
@@ -129,4 +196,5 @@ struct GameView: View {
 
 #Preview {
     GameView()
+        .environmentObject(ScoutingController())
 }
