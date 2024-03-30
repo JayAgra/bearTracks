df1ca02263bdcf4a320482d41529a796333f9ec2
JayAgra <69493224+JayAgra@users.noreply.github.com>
2024-03-29 17:40:56 -0700

    remove APN scraps

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-use a2;
 use actix_governor::{Governor, GovernorConfigBuilder};
 use actix_http::StatusCode;
 use actix_identity::{CookieIdentityPolicy, Identity, IdentityService};
@@ -20,16 +19,14 @@ use openssl::{
 use r2d2_sqlite::{self, SqliteConnectionManager};
 use reqwest::Client;
 use serde::{Deserialize, Serialize};
-use std::{collections::HashMap, env, fs, io, pin::Pin, sync::{Arc, RwLock}};
+use std::{collections::HashMap, env, fs, io, pin::Pin, sync::RwLock};
 use webauthn_rs::prelude::*;
 
 mod analyze;
-mod apn;
 mod auth;
 mod casino;
 mod db_auth;
 mod db_main;
-mod db_pit;
 mod db_transact;
 mod forward;
 mod game_api;
@@ -602,18 +599,6 @@ async fn game_get_team(req: HttpRequest, db: web::Data<Databases> /*, _user: db_
     ))
 }
 
-#[derive(Serialize)]
-struct ApiResponse {
-    data: String,
-}
-
-async fn trigger_notification(client: web::Data<Arc<a2::Client>>, device_token: web::Path<String>) -> impl Responder {
-    match apn::send_notification(client.get_ref(), device_token.as_str()).await {
-        Ok(_) => HttpResponse::Ok().json(ApiResponse { data: "Notification sent.".to_string() }),
-        Err(err) => HttpResponse::InternalServerError().body(err.to_string()),
-    }
-}
-
 include!(concat!(env!("OUT_DIR"), "/generated.rs"));
 
 #[actix_web::main]
@@ -743,16 +728,6 @@ async fn main() -> io::Result<()> {
     let intermediate_cert = X509::from_der(&intermediate_bytes).unwrap();
     builder.add_extra_chain_cert(intermediate_cert).unwrap();
 
-    let client = match apn::setup_client() {
-        Ok(client) => client,
-        Err(err) => {
-            log::error!("Failed to setup client on error: {}", err);
-            std::process::exit(1)
-        },
-    };
-
-    let client = Arc::new(client);
-
     // config done. now, create the new HttpServer
     log::info!("[OK] starting bearTracks on port 443 and 80");
 
@@ -771,7 +746,6 @@ async fn main() -> io::Result<()> {
             // add webauthn to app data
             .app_data(passkey::setup_passkeys())
             // apn client
-            .app_data(web::Data::new(client.clone()))
             // use governor ratelimiting as middleware
             .wrap(Governor::new(&governor_conf))
             // use cookie id system middleware
@@ -826,7 +800,10 @@ async fn main() -> io::Result<()> {
             .service(ResourceFiles::new("/static", generated))
             /* auth endpoints */
             // GET
-            .service(web::resource("/logout").route(web::get().to(auth_get_logout)))
+            .service(
+                web::resource("/logout")
+                .route(web::get().to(auth_get_logout))
+            )
             // POST
             .service(
                 web::resource("/api/v1/auth/create")
@@ -1017,10 +994,6 @@ async fn main() -> io::Result<()> {
                 web::resource("/api/v1/debug/ok")
                     .route(web::get().to(debug_ok)),
             )
-            .service(
-                web::resource("/api/v1/trigger_notification/{device_token}")
-                    .route(web::get().to(trigger_notification)),
-            )
             /* robot game endpoints */
             // GET
             .service(
@@ -1044,8 +1017,10 @@ async fn main() -> io::Result<()> {
                     .route(web::get().to(game_open_lootbox)),
             )
             // POST
-            .service(web::resource("/api/v1/game/set_hand")
-                .route(web::post().to(game_set_hand)))
+            .service(
+                web::resource("/api/v1/game/set_hand")
+                    .route(web::post().to(game_set_hand))
+            )
     })
     .bind_openssl(format!("{}:443", env::var("HOSTNAME").unwrap_or_else(|_| "localhost".to_string())), builder)?
     .bind((env::var("HOSTNAME").unwrap_or_else(|_| "localhost".to_string()), 80))?
